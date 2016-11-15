6f3f0a3995f9737996a5f8e435d78d0e9400aba4
twilson20 <tkw266@nyu.edu>
2016-11-15 10:20:08 -0500

    New SS8

--- SS8/sketch.js
+++ SS8/sketch.js
@@ -3,38 +3,62 @@
 // cloud = http://www.clipartbest.com/cloud-png
 // data = http://www.weather.gov/media/okx/Climate/CentralPark/monthlyannualtemp.pdf
 
-var weather; //able and images 
+var weather; //table and images 
 var sun;
 var cloud;
 var snow;
 
 function preload () {
-  weather = loadTable("data.csv"); // loading the table and images 
+  weather = loadTable("data.csv","header"); // loading the table and images 
   sun = loadImage("sun.png")
   cloud = loadImage("cloud.png")
   snow = loadImage("snow.png")
 }
 
 function setup() {
-  createCanvas(2000,1800); 
+  createCanvas(800,800); 
   background(0);
-  for (var i = 0; i < weather.getRow(); i++) {
-  var temp = weather.get(0);
+  
+  var row = weather.getRow(0); // get data from row 0 
+  
+  for (var i = 0; i < weather.getColumnCount(); i++) {
+  var temp = row.getNum(i);
+  console.log(temp);
   }
 }
 
+// function draw () {
+//   var row = weather.getRow(0);
+  
+//   for (var i = 0; i < weather.getColumnCount(); i++) {
+//   var temp = row.getNum(i);
+//   if (temp < 35) {
+//   image (snow, temp,temp,100,100);
+//   }
+//   if (temp >= 35 || i <= 60) {
+//   image (cloud, temp, temp,100,100);
+//   }
+//   if (temp >  60) {
+//   image (sun, temp, temp,100,100);
+//   }
+// }
+// }
+
 function draw () {
-  for (var i = 0; i < weather.getRowCount(); i++) {
-  var temp = weather.getRow(0);
-  if (i < 35) {
-  image (snow, i, i);
-  snow.resize(100,100);
-  } else if (i >= 35 && i <= 60) {
-  image (cloud, i, i );
-  cloud.resize (100,100);p
-  } else if (i >  60) {
-  image (sun, i, i );
-  sun.resize(100,100);
+  var row = weather.getRow(0); //row with numbers 
+  
+  for (var i = 0; i < weather.getColumnCount(); i++) {
+  var temp = row.getNum(i);
+  if (temp < 35) {
+  image (snow, temp,temp,100,100);
+  }
+  if (temp >= 35 || i <= 60) {
+  image (cloud, temp, temp,100,100);
+  }
+  if (temp >  60) {
+  image (sun, temp, temp,100,100);
   }
 }
 }
+
+
