3e542f7ccc88b1eac9c500b24add2c9f85da9f6e
twilson20 <tkw266@nyu.edu>
2016-11-22 01:14:23 -0500

    SS8 Data Visualization

--- SS8_Tejah/sketch.js
+++ SS8_Tejah/sketch.js
@@ -1,59 +1,57 @@
-// pulling data from http://www.cdc.gov/nchs/pressroom/sosmap/firearm_mortality/firearm.htm 
+// snow = http://pngimg.com/img/nature/snowflakes
+// sun = http://www.pngall.com/sun-png
+// cloud = http://www.clipartbest.com/cloud-png
+// data = http://www.weather.gov/media/okx/Climate/CentralPark/monthlyannualtemp.pdf
 
-// declare arrays for holding column data 
-
-var states =  [];
-var rates = [];
-var deaths = [];
-
+var weather; //table and images 
+var sun;
+var cloud;
+var snow;
+var data = [];
 function preload () {
-  firearms = loadTable ("firearms2014.csv", "header");
+  weather = loadTable("data.csv","header"); // loading the table and images 
+  sun = loadImage("sun.png")
+  cloud = loadImage("cloud.png")
+  snow = loadImage("snow.png")
 }
 
 function setup() {
-  createCanvas (1400, 800);
-  background(255);
-  // printing out data to the console
-  
-  // how many rows do you have ?
-  console.log(firearms.getRowCount());
-  
-  // how many columns?
-  console.log(firearms.getColumnCount());
-  
-  // printing the states
-  console.log (firearms.getColumn("STATE")); // call by the header or by column number 
-  
-  //print out stats for california 
-  console.log (firearms.get(4,0)); // 4th row, 1st column (starts at 0)
-  console.log(firearms.get(4,1));
-  console.log(firearms.get(4,2));
+  createCanvas(1400,700); 
+  background(0);
 
-  //assign values to arrays for each colum in state 
-  for (var i =0; i < firearms.getRowCount();i ++){
-    states [i] = firearms.getString(i,0);
-    rates [i] = firearms.getNum (i,1);
-    deaths [i] = firearms.getNum (i,2); 
+  var row = weather.getRow(0); // get data from row 0 
+  var labels = weather.getRow(1);
+  console.log(weather.getRowCount());
+  for (var i = 0; i < weather.getColumnCount(); i++) {
+  var temp = row.getNum(i);
+
+  console.log(temp);
   }
-  console.log(states[1]);
 }
 
-
-function draw() {
-  // displaying the states 
-  background (255);
-  textSize(14);
-  textAlign(CENTER);
-  ellipseMode(CENTER);
-  for (var i =0; i <states.length; i ++){
-    text (states [i], i*27 + 27, height/2);
-    ellipse(i*27+27, deaths[i],10,10);
+function draw () {
+  textSize(30);
+  var row = weather.getRow(0); //row with numbers 
+  var labels = weather.getRow(1);
+  fill (255);
+  text (labels.getString(12)+ " " + row.getNum(12), 30, 200);
+  text ("Average Monthly Temperature: Central Park 2015",300 , 100)
+  for (var i = 0; i < weather.getColumnCount()-1; i++) {
+  var temp = row.getNum(i);
+  var x = 20 + 100 * i ;
+  var y = 500 - 3 * temp;
+  if (temp < 35) {
+  image (snow,x ,y,100,100);
+  }
+  if (temp >= 35 && temp <= 60) {
+  image (cloud, x, y,100,100);
+  }
+  if (temp >  60) {
+  image (sun, x, y,100,100);
+  }
+  text (labels.getString(i),x+23,600);
   }
 }
 
 
 
-
-
-
-  
